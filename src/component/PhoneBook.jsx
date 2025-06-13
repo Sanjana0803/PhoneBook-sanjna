116b4d40212a57e534387ddbed98e59bf288de9a
Sanjana0803 <sanjanasolanki5820@gmail.com>
2025-06-13 15:23:16 +0530

    stable

--- src/component/PhoneBook.jsx
+++ src/component/PhoneBook.jsx
@@ -17,6 +17,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import BookmarkAddTwoToneIcon from '@mui/icons-material/BookmarkAddTwoTone';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
+import { Avatar } from '@mui/material';
 
 
 
@@ -34,10 +35,31 @@ function PhoneBook() {
 
     const [contacts, setContacts] = useState([]);
     const [searchFilter, setSearchFilter] = useState('');
-    const [image, setImage] = useState('')
-    const[Edit,setEdit]=useState(false);
-    const[Editindex,setEditindex]=useState('null');
-    
+    const [Edit, setEdit] = useState(false);
+    const [Editindex, setEditindex] = useState('null');
+    const [imageFile, setimageFile] = useState(null);
+    const [imageUrl, setimageUrl] = useState('');
+    const [currentPage, setcurrentPage] = useState(1);
+
+    // it will be contacts per page on single page 
+    const pageSize = 5;
+
+
+    const totalcontacts = contacts.length;
+    const indexofLastContact = currentPage * pageSize;
+    const indexofFirstContact = indexofLastContact - pageSize;
+
+
+
+    const handlePageChange = (pageNumber) => {
+        setcurrentPage(pageNumber);
+    };
+
+    const pageNumber = [];
+    for (let i = 1; i <= Math.ceil(totalcontacts / pageSize); i++) {
+        pageNumber.push(i);
+    }
+
 
 
     // fetching the data from localstorage if we reload it
@@ -60,31 +82,58 @@ function PhoneBook() {
         setAddress('');
         setPhoneNumber('');
         setLabel('');
+        setimageUrl('');
     }
+    const handleImageUpload = async (e) => {
+        const file = e.target.files[0];
+        setimageFile(file);
 
-  
+
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append('upload_preset', "my_unsigned_upload")
+        console.log("FormData", formData.file)
+
+        try {
+            // CLOUDINARY_URL=cloudinary://<your_api_key>:<your_api_secret>@dzlf5xfe3
+            const response = await fetch('https://api.cloudinary.com/v1_1/dzlf5xfe3/image/upload', {
+                // const response = await fetch('https://cloudinary://477794365294579:zvaEXmvilS4rACgptj39fYIkXHs@dzlf5xfe3', {
+                method: "POST",
+                body: formData
+            });
+            const data = await response.json();
+            setimageUrl(data.secure_url);
+            console.log(data);
+            console.log("Image URL:", data.secure_url);
+            setimageUrl(data.secure_url);
+        }
+        catch (error) {
+            console.log("this is error", error);
+        }
+    };
+
     const handleAddorEditContact = () => {
         if (name && phonenumber && label) {
-            const newContact = { name, phonenumber, address, label };
+            const newContact = { name, phonenumber, address, label, imageUrl };
 
-            if(Edit){
+            if (Edit) {
                 // edit pr index check krke edit krga 
-                const updatedContancts=[...contacts];
-                updatedContancts[Editindex]=newContact;
+                const updatedContancts = [...contacts];
+                updatedContancts[Editindex] = newContact;
                 setContacts(updatedContancts);
-                localStorage.setItem('contacts',JSON.stringify(updatedContancts));
+                localStorage.setItem('contacts', JSON.stringify(updatedContancts));
             }
-            else{
+            else {
                 // add pr naya contact
-                const updatedContacts=[...contacts,newContact];
+                const updatedContacts = [...contacts, newContact];
                 setContacts(updatedContacts);
-                localStorage.setItem('contacts',JSON.stringify(updatedContacts));
+                localStorage.setItem('contacts', JSON.stringify(updatedContacts));
             }
             handleClose();
         }
     };
 
-  
+
 
     const handleDelete = (index) => {
         const updatedContacts = contacts.filter((_, i) => i !== index);
@@ -92,17 +141,20 @@ function PhoneBook() {
         localStorage.setItem('contacts', JSON.stringify(updatedContacts))
     }
 
-   const handleEdit=(index)=>{
-    // set existing data on edit
-    const contact=contacts[index];
-    setName(contact.name);
-    setPhoneNumber(contact.phonenumber);
-    setAddress(contact.address);
-    setLabel(contact.label);
-    setEditindex(index);
-    setEdit(true);
-    setOpen(true);
-}
+    const handleEdit = (index) => {
+        // set existing data on edit
+        const contact = contacts[index];
+        setName(contact.name);
+        setPhoneNumber(contact.phonenumber);
+        setAddress(contact.address);
+        setLabel(contact.label);
+        // setimageFile(contacts.imageFile);
+        setimageUrl(contact.imageUrl)
+        setEditindex(index);
+        setEdit(true);
+        setOpen(true);
+
+    }
     const filterContacts = contacts.filter(contact => {
         const filtereddata = contact.name.toLowerCase().includes(searchFilter.toLowerCase())
         // console.log(filtereddata);
@@ -110,6 +162,7 @@ function PhoneBook() {
         // console.log(filteredlabel);
         return filtereddata && filteredlabel;
     })
+
 
 
 
@@ -185,8 +238,8 @@ function PhoneBook() {
             </Box>
             <Dialog open={open} >
                 <DialogTitle>
-                   {Edit?'Edit Contact':'New Contact'}
-                    </DialogTitle>
+                    {Edit ? 'Edit Contact' : 'New Contact'}
+                </DialogTitle>
                 <DialogContent sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
                     <TextField
                         label='Name'
@@ -211,9 +264,8 @@ function PhoneBook() {
                     </TextField>
 
                     <TextField
-                        label='Image'
-                        value={image}
-                        onChange={(e) => setImage(e.target.value)}
+                        // label='Image'
+                        onChange={handleImageUpload}
                         type='file'>
                     </TextField>
 
@@ -232,46 +284,102 @@ function PhoneBook() {
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleAddorEditContact} variant='contained'>
-                        {Edit?'Save Changes':'Add Contact'}
-                        </Button>
+                        {Edit ? 'Save Changes' : 'Add Contact'}
+                    </Button>
                     <Button onClick={handleClose}>
                         <Close />
                     </Button>
                 </DialogActions>
             </Dialog>
             <Box sx={{ marginTop: 4 }}>
-                <TableContainer component={Paper}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell><strong>Name</strong></TableCell>
-                                <TableCell><strong>Phone</strong></TableCell>
-                                <TableCell><strong>Actions</strong></TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {filterContacts.map((contacts, index) => (
-                                <TableRow key={index}>
-                                    <TableCell>{contacts.name}</TableCell>
-                                    <TableCell>{contacts.phonenumber}</TableCell>
-                                    <TableCell>
-                                        <EditIcon onClick={() => handleEdit(index)} />
-                                        <DeleteIcon onClick={() => handleDelete(index)} />
-                                        <BookmarkAddTwoToneIcon />
 
-                                    </TableCell>
 
+
+                <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+                    <TableContainer component={Paper} sx={{ width: '80%' }}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell><strong>Name</strong></TableCell>
+                                    <TableCell><strong>Phone</strong></TableCell>
+                                    <TableCell><strong>Actions</strong></TableCell>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                            </TableHead>
+                            {/* <TableBody>
+                                {filterContacts.map((contacts, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell>
+                                            <Box sx={{ display: 'flex', textAlign: 'center' }}>
+                                                {
+                                                    contacts.imageUrl && (
+                                                        <Avatar
+                                                            src={contacts.imageUrl}
+                                                            alt='avatar'
+                                                            sx={{ width: 32, height: 32, marginRight: 1 }}
+                                                        />
+                                                    )}
+                                                {contacts.name}
+                                            </Box>
+                                        </TableCell>
+                                        <TableCell>{contacts.phonenumber}</TableCell>
+                                        <TableCell>
+                                            <EditIcon onClick={() => handleEdit(index)} />
+                                            <DeleteIcon onClick={() => handleDelete(index)} />
+                                            <BookmarkAddTwoToneIcon sx={{
+                                                ":hover": {
+                                                    color: '#000000'
+                                                }
+                                            }}>
 
-                <Box sx={{maxWidth:800,display:'flex',justifyContent:'center'}}>
+                                            </BookmarkAddTwoToneIcon>
 
-                    <Stack spacing={2}>
-                        <Pagination count={5} size="large" />
-                    </Stack>
+                                        </TableCell>
+
+                                    </TableRow>
+                                ))}
+                            </TableBody> */}
+
+                            <TableBody>
+                                {filterContacts.slice(indexofFirstContact, indexofLastContact).map((contact, index) => (
+                                    <TableRow key={index}>
+                                        <TableCell>
+                                            <Box sx={{ display: 'flex', textAlign: 'center' }}>
+                                                {contact.imageUrl && (
+                                                    <Avatar
+                                                        src={contact.imageUrl}
+                                                        alt='avatar'
+                                                        sx={{ width: 32, height: 32, marginRight: 1 }}
+                                                    />
+                                                )}
+                                                {contact.name}
+                                            </Box>
+                                        </TableCell>
+                                        <TableCell>{contact.phonenumber}</TableCell>
+                                        <TableCell>
+                                            <EditIcon onClick={() => handleEdit(index+indexofFirstContact)} />
+                                            <DeleteIcon onClick={() => handleDelete(index+indexofLastContact)} />
+                                            <BookmarkAddTwoToneIcon sx={{
+                                                ":hover": {
+                                                    color: '#000000'
+                                                }
+                                            }} />
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+
+                        </Table>
+                    </TableContainer>
+                </Box>
+
+                <Box sx={{ maxWidth: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <>
+                        <Pagination
+                            count={pageNumber.length}
+                            page={currentPage}
+                            onChange={(e, p) => handlePageChange(p)}
+                        />
+                    </>
                 </Box>
 
             </Box>
